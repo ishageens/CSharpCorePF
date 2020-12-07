444bd4b933b4361e2667b96469fd72bfefab04be
Isha <ishageens@msn.com>
2020-12-07 12:49:54 +0100

    Oefening Conversie Celcius -> Fahrenheit

---
+++ CSharpCorePFOefenmap/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSharpCorePFOefenmap
+{
+    class Program
+    {
+        const float GemLichTempCelcius = 37.0F;
+        static void Main(string[] args)
+        {
+            float GemLichTemptFahrenheit =
+                GemLichTempCelcius * 9.0F / 5.0F + 32.0F;
+            Console.WriteLine("Gemiddelde lichaamstemperatuur in graden Celcius: " + GemLichTempCelcius);
+            Console.WriteLine("Gemiddelde lichaamstemperatuur in graden Fahrenheit: " + GemLichTemptFahrenheit);
+
+
+
+        }
+    }
+}
