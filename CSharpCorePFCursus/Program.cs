ffb3ce0434a85d82afe114adfbaaa6b89ae0124b
Isha <ishageens@msn.com>
2020-12-11 09:32:54 +0100

    Cursus selecties

--- CSharpCorePFCursus/Program.cs
+++ CSharpCorePFCursus/Program.cs
@@ -6,20 +6,12 @@ namespace CSharpCorePFCursus
     {
         static void Main(string[] args)
         {
-           byte statenUSA = 50;
-           ushort belgischeBieren = 1400;
-            uint afstandNaarMaan = 382170;
-            ulong aantalMensen = 6122567014ul;
-           float alcoholDuvel = 8.5f;
-           char geslacht = 'M';
-           bool gehuwd = true;
-            Console.WriteLine(statenUSA);
-            Console.WriteLine(belgischeBieren);
-            Console.WriteLine(afstandNaarMaan);
-            Console.WriteLine(aantalMensen);
-            Console.WriteLine(alcoholDuvel);
-            Console.WriteLine(geslacht);
-            Console.WriteLine(gehuwd);
+            Console.Write("Tik je leeftijd:");
+            int leeftijd = int.Parse(Console.ReadLine());
+            Console.Write("Je bent");
+            if (leeftijd < 18)
+                Console.Write(" niet");
+            Console.WriteLine(" toegelaten.");
         }
     }
 }
