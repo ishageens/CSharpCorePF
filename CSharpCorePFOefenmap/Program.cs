b18fcbe4ea01ebb6dc156cabcfbee83f569cd2d4
Isha <ishageens@msn.com>
2020-12-11 10:04:15 +0100

    Oefening LichtKrant

--- CSharpCorePFOefenmap/Program.cs
+++ CSharpCorePFOefenmap/Program.cs
@@ -4,19 +4,37 @@ namespace CSharpCorePFOefenmap
 {
     class Program
     {
+        const string BoodschapWeekdag = "We wensen u een prettige werkdag!";
+        const string BoodschapWeekend = "We wensen u een fijn weekend!";
+        const string OpeningsurenWeekdag = "9u00-12u00 en 13u00-18u00";
+        const string OpeningsurenZaterdag = "10u00-12u00";
+        const string OpeningsurenZondag = "Gesloten";
         static void Main(string[] args)
         {
-            Console.Write("geef een jaartal");
-            int jaar = int.Parse(Console.ReadLine());
+            Console.Write("Datum? ");
+            DateTime datum = DateTime.Parse(Console.ReadLine());
+            System.Text.StringBuilder boodschap = new System.Text.StringBuilder("Openingsuren: ");
 
-            string schrikkel;
-            if (jaar % 4 == 0)
-                if (jaar % 100 == 0 && jaar % 400 != 0)
-                    schrikkel = "geen";
-                else schrikkel = "een";
-            else
-                schrikkel = "geen";
-            Console.WriteLine("Het jaar {0} is {1} schrikkeljaar", jaar, schrikkel);
+            switch (datum.DayOfWeek)
+            {
+                case DayOfWeek.Monday:
+                case DayOfWeek.Tuesday:
+                case DayOfWeek.Wednesday:
+                case DayOfWeek.Thursday:
+                case DayOfWeek.Friday:
+                    boodschap.AppendLine(OpeningsurenWeekdag);
+                    boodschap.Append(BoodschapWeekdag);
+                    break;
+                case DayOfWeek.Saturday:
+                    boodschap.Append(OpeningsurenZaterdag + Environment.NewLine + BoodschapWeekend);
+                    break;
+                case DayOfWeek.Sunday:
+                    boodschap.Clear();
+                    boodschap.Append(OpeningsurenZondag + Environment.NewLine + BoodschapWeekend);
+                    break;
+                default: break;
+            }
+            Console.WriteLine(boodschap.ToString());
         }
     }
 }
