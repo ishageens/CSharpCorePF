bde1f9904516c834c271ea9c6e533c5273595fde
Isha <ishageens@msn.com>
2020-12-14 12:46:37 +0100

    added werknemer class

---
+++ CSharpCorePFCursus/Werknemer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpCorePFCursus
+{
+    public class Werknemer
+    {
+        private string naamValue;
+        public string Naam
+        {
+            get { return naamValue; }
+            set {
+                if (value != string.Empty)
+                    naamValue = value; }
+        }
+
+        private DateTime inDienstValue;
+        public DateTime InDienst
+        {
+            get { return inDienstValue; }
+            set { inDienstValue = value; }
+        }
+
+        private Geslacht geslachtValue;
+        public Geslacht Geslacht
+        {
+            get { return geslachtValue; }
+            set { geslachtValue = value; }
+        }
+    }
+}
