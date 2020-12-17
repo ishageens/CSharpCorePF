92c1e1b5cf84b9693a0003a0238337b231d2f1a3
Isha <ishageens@msn.com>
2020-12-17 14:41:57 +0100

    methods en parameters

---
+++ CSharpCorePFCursus/LijnenTrekker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpCorePFCursus
+{
+    class LijnenTrekker
+    {
+        public void TrekLijn1(int lengte, char teken)
+        {
+            for (int teller = 0; teller < lengte; teller++)
+                Console.Write(teken);
+            Console.WriteLine();
+        }
+
+        public void TrekLijn2(int lengte)
+        {
+            for (int teller = 0; teller < lengte; teller++)
+                Console.Write('-');
+            Console.WriteLine();
+        }
+
+        public void TrekLijn3()
+        {
+            for (int teller = 0; teller < 80; teller++)
+                Console.Write('-');
+            Console.WriteLine();
+        }
+    }
+}
