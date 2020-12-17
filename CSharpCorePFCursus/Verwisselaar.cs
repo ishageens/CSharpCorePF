c82f47b12f9b36c3e5aee59f6dfcdfe51c7f0db5
Isha <ishageens@msn.com>
2020-12-17 14:41:37 +0100

    ref and out

---
+++ CSharpCorePFCursus/Verwisselaar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpCorePFCursus
+{
+    public class Verwisselaar
+    {
+        public void Verwissel(ref int getal1, ref int getal2)
+        {
+            int tussen = getal1;
+            getal1 = getal2;
+            getal2 = tussen;
+        }
+    }
+}
