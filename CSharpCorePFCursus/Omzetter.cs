8aabb57cc8fc739f2385aec98c55b08b280b0c7f
Isha <ishageens@msn.com>
2020-12-17 15:12:12 +0100

    methods met resultaatwaarde

---
+++ CSharpCorePFCursus/Omzetter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpCorePFCursus
+{
+    class Omzetter
+    {
+        public const double CentimetersPerInch = 2.54;
+        public double CmNaarInch(double cm)
+        {
+            return cm / CentimetersPerInch;
+        }
+        public double InchNaarCm(double inch)
+        {
+            return inch * CentimetersPerInch;
+        }
+    }
+}
