6db9d337a263c90b13357fe02970ad15c0a145d9
Isha <ishageens@msn.com>
2020-12-18 13:57:06 +0100

    Oefening Bank

--- CSharpCorePFOefenmap/Program.cs
+++ CSharpCorePFOefenmap/Program.cs
@@ -4,37 +4,13 @@ namespace CSharpCorePFOefenmap
 {
     class Program
     {
-        const string BoodschapWeekdag = "We wensen u een prettige werkdag!";
-        const string BoodschapWeekend = "We wensen u een fijn weekend!";
-        const string OpeningsurenWeekdag = "9u00-12u00 en 13u00-18u00";
-        const string OpeningsurenZaterdag = "10u00-12u00";
-        const string OpeningsurenZondag = "Gesloten";
         static void Main(string[] args)
         {
-            Console.Write("Datum? ");
-            DateTime datum = DateTime.Parse(Console.ReadLine());
-            System.Text.StringBuilder boodschap = new System.Text.StringBuilder("Openingsuren: ");
+            Rekening rekening = new Rekening("BE19731021964512", 100, DateTime.Today);
+            rekening.Afbeelden();
+            rekening.Storten(100m);
+            rekening.Afbeelden();
 
-            switch (datum.DayOfWeek)
-            {
-                case DayOfWeek.Monday:
-                case DayOfWeek.Tuesday:
-                case DayOfWeek.Wednesday:
-                case DayOfWeek.Thursday:
-                case DayOfWeek.Friday:
-                    boodschap.AppendLine(OpeningsurenWeekdag);
-                    boodschap.Append(BoodschapWeekdag);
-                    break;
-                case DayOfWeek.Saturday:
-                    boodschap.Append(OpeningsurenZaterdag + Environment.NewLine + BoodschapWeekend);
-                    break;
-                case DayOfWeek.Sunday:
-                    boodschap.Clear();
-                    boodschap.Append(OpeningsurenZondag + Environment.NewLine + BoodschapWeekend);
-                    break;
-                default: break;
-            }
-            Console.WriteLine(boodschap.ToString());
         }
     }
 }
---
+++ CSharpCorePFOefenmap/Rekening.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpCorePFOefenmap
+{
+    class Rekening
+    {
+        private string rekeningNummerValue;
+        private readonly DateTime EersteCreatie = new DateTime(1900, 1, 1);
+        private DateTime creatieDatumValue;
+
+        public string RekeningNummer
+        {
+            get { return rekeningNummerValue; }
+            set
+            {
+                if (IsGeldigRekeningNummer(value))
+                    rekeningNummerValue = value;
+            }
+        }
+
+        public decimal Saldo { get; set; }
+
+        public DateTime CreatieDatum
+        {
+            get { return creatieDatumValue; }
+            set
+            {
+                if (value >= EersteCreatie)
+                    creatieDatumValue = value;
+            }
+        }
+
+        public Rekening(string rekeningNummer, decimal saldo, DateTime creatieDatum)
+        {
+            this.RekeningNummer = rekeningNummer;
+            this.Saldo = saldo;
+            this.CreatieDatum = creatieDatum;
+        }
+
+        public void Afbeelden()
+        {
+            Console.WriteLine($"Rekeningnummer: {RekeningNummer}");
+            Console.WriteLine($"Saldo: {Saldo}");
+            Console.WriteLine($"Creatiedatum: {CreatieDatum}");
+        }
+        public void Storten(decimal bedrag)
+        {
+            Saldo += bedrag;
+        }
+
+        private bool IsGeldigRekeningNummer(string rekeningNummer)
+        {
+            if (string.IsNullOrWhiteSpace(rekeningNummer))
+                return false;
+            if (rekeningNummer.Length != 16)
+                return false;
+            if (rekeningNummer.Substring(0, 2) != "BE")
+                return false;
+            if (!int.TryParse(rekeningNummer.Substring(2, 2), out int derdevierdeteken))
+                return false;
+            if (!ulong.TryParse(rekeningNummer.Substring(4, 12), out ulong belgischRekeningNummer))
+                return false;
+            ulong eerste10 = belgischRekeningNummer / 100ul;
+            int laatste2 = (int)(belgischRekeningNummer % 100ul);
+            return (int)(eerste10 % 97ul) == laatste2;
+
+        }
+    }
+}
