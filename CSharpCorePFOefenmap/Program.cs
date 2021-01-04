71001da61c97c2dc38750a26b3d4df98421d4d41
Isha <ishageens@msn.com>
2021-01-04 09:59:43 +0100

    Oefening inheritance polymorphisme

--- CSharpCorePFOefenmap/Program.cs
+++ CSharpCorePFOefenmap/Program.cs
@@ -6,17 +6,11 @@ namespace CSharpCorePFOefenmap
     {
         static void Main(string[] args)
         {
-            Vrachtwagen vw1 = new Vrachtwagen("Jos", 125000, 250, 1000, "1abc758", 2500);
-            vw1.Afbeelden();
-
-            Vrachtwagen vw2 = new Vrachtwagen();
-            vw2.Afbeelden();
-
-            Personenwagen pw1 = new Personenwagen("Jef", 85000, 100, 600, "1def654", 5, 5);
-            pw1.Afbeelden();
-
-            Personenwagen pw2 = new Personenwagen();
-            pw2.Afbeelden();
+            Rekening[] rekeningen = new Rekening[2];
+            rekeningen[0] = new Spaarrekening("BE19731021964512", 200, DateTime.Today);
+            rekeningen[1] = new Zichtrekening("BE19731021964512", 100, DateTime.Today, -750);
+            foreach (Rekening eenRekening in rekeningen)
+                eenRekening.Afbeelden();
 
         }
     }
