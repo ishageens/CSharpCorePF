171d8c9b2dea394ed19fd6d79107fd60765ffa55
Isha <ishageens@msn.com>
2021-01-12 09:34:15 +0100

    Hoofdstuk nullable types

--- CSharpCorePFCursus/Program.cs
+++ CSharpCorePFCursus/Program.cs
@@ -9,15 +9,10 @@ namespace CSharpCorePFCursus
     {
         public static void Main(string[] args)
         {
-            List<Werknemer> werknemers = new List<Werknemer>
-            { new Arbeider("Asterix", DateTime.Today, Geslacht.Man, 25m, 1),
-                new Bediende("Obelix", DateTime.Today, Geslacht.Man, 1200m)
-            };
-            Console.WriteLine($"Aantal werknemers: {werknemers.Count}");
-
-            werknemers = null; //NullReferenceException 
-            //Console.WriteLine($"Aantal werknemers: {werknemers.Count}");
-            Console.WriteLine($"Aantal werknemers: {werknemers?.Count ?? 0}");
+            byte? aantalKinderen = null;
+            byte aantalKamers;
+            aantalKamers = aantalKinderen ?? 0;
+            Console.WriteLine($"Er zijn {aantalKamers} kinderkamers nodig");
         }
     }
 }
