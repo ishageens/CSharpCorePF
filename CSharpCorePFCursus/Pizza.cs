db3da35948bdd5846ef5adb1283e07c470a520fb
Isha <ishageens@msn.com>
2021-01-12 20:34:29 +0100

    Hoofdstuk Files and streams

---
+++ CSharpCorePFCursus/Pizza.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CSharpCorePFCursus
+{
+    [Serializable]
+    class Pizza
+    {
+        public string Naam { get; set; }
+        public List<String> Onderdelen { get; set; }
+        public decimal Prijs { get; set; }
+
+        public override string ToString() => $"{this.Naam}: {this.Prijs} EUR";
+    }
+}
--- CSharpCorePFCursus/Program.cs
+++ CSharpCorePFCursus/Program.cs
@@ -1,23 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Firma;
-using Firma.Personeel;
-using Firma.Materiaal;
+using System.IO;
+using System.Text;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+
 namespace CSharpCorePFCursus
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            var brouwers = new Brouwers().GetBrouwers();
-            var opBelgisch = from brouwer in brouwers
-                             group brouwer by brouwer.Belgisch;
-            foreach (var welNietBelgisch in opBelgisch)
+            try
             {
-                Console.WriteLine(welNietBelgisch.Key ? "Belgisch" : "Niet-Belgisch");
-                foreach (var brouwer in welNietBelgisch)
-                    Console.WriteLine(brouwer.Brouwernaam);
+                List<Pizza> pizzas;
+                using var bestand = File.Open(@"C:\Data\Pizzas.obj",
+                FileMode.Open, FileAccess.Read);
+                var lezer = new BinaryFormatter();
+                pizzas = (List<Pizza>)lezer.Deserialize(bestand);
+                foreach (var pizza in pizzas)
+                {
+                    Console.WriteLine(pizza.Naam);
+                    foreach (var onderdeel in pizza.Onderdelen)
+                        Console.WriteLine(onderdeel);
+                    Console.WriteLine(pizza.Prijs);
+                }
+            }
+            catch (IOException)
+            {
+                throw new Exception("Fout bij het openen van het bestand!");
+            }
+            catch (SerializationException)
+            {
+                Console.WriteLine("Fout bij het serialiseren/deserialiseren");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
             }
         }
     }
